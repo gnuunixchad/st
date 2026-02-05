cbcb042158c50528e7f5d65b73da53339a5dcd80
nate zhou <gnuunixchad@outlook.com>
2026-02-05 23:38:20 +0800

    Mod(config.def.h): customize
    - font family and size
    - bindings for scroll, zoom, copyurl

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -228,21 +228,22 @@ static Shortcut shortcuts[] = {
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ ShiftMask,            XK_Home,        kscrollup,      {.i = 1000000} },
-	{ ShiftMask,            XK_End,         kscrolldown,    {.i = 1000000} },
+	{ ControlMask|ShiftMask,XK_K,           kscrollup,      {.i = -1} },
+	{ ControlMask|ShiftMask,XK_J,           kscrolldown,    {.i = -1} },
+	{ ControlMask|ShiftMask,XK_P,           kscrollup,      {.i = -20} },
+	{ ControlMask|ShiftMask,XK_N,           kscrolldown,    {.i = -20} },
+	{ ControlMask|ShiftMask,XK_T,           kscrollup,      {.i = 1000000} },
+	{ ControlMask|ShiftMask,XK_B,           kscrolldown,    {.i = 1000000} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
+	{ ControlMask|ShiftMask,XK_I,           zoom,           {.f = +1} },
+	{ ControlMask|ShiftMask,XK_O,           zoom,           {.f = -1} },
+	{ ControlMask|ShiftMask,XK_Z,           zoomreset,      {.f =  0} },
+	{ ControlMask|ShiftMask,XK_C,           clipcopy,       {.i =  0} },
+	{ ControlMask|ShiftMask,XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_l,           copyurl,        {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_L,           copyurl,        {.i =  1} }
+	{ ControlMask|ShiftMask,XK_L,           copyurl,        {.i =  1} }
 };
 
 /*
